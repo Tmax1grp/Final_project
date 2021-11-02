790a3640f4fef6fb88a752415b9fcd195a9582a2
Alexhwang95 <mdok3488@naver.com>
2021-11-02 09:30:15 +0900

    게시글 수정

--- class-frontend-service/src/components/Board/ClassBoardDiscuss.js
+++ class-frontend-service/src/components/Board/ClassBoardDiscuss.js
@@ -1,11 +1,54 @@
-import React from 'react';
-import ClassBoardList from '../widgets/ClassBoardList';
+import React,{useEffect, useState} from 'react';
+import ClassBoardList from '../widgets/ClassBoardList'
+import axios from 'axios';
+
 
 // TODO: 한 페이지 게시글 최대 개수 지정 혹은 스크롤링
 export default function ClassBoardDiscuss({ classId }) {
+
+    const [classes, setClasses] = useState([]);
+
+
+    useEffect(() => {
+        axios.get(`/${classId}/discuss/findall`)
+        .then(res => {
+            // console.log(res.data)
+            setClasses(res.data);
+        })
+        .catch((err) =>
+          console.log(err)
+        )
+      },[classId])
+      const classeslist = classes.map((clas) => {
+        return (
+    <tr>
+      <th scope="row">{clas.discussId}</th>
+      <td>{clas.title}</td>
+      <td>{clas.author}</td>
+      <td>{clas.createDate}</td>
+    </tr>
+            
+        )
+    })
+
     return (
         <div>
-            <ClassBoardList name="discuss" classId={classId} />
+            <table class="table table-sm">
+  <thead>
+    <tr>
+      <th scope="col">목록</th>
+      <th scope="col">제목</th>
+      <th scope="col">작성자</th>
+      <th scope="col">날짜</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+      {classeslist}
+   
+  </tbody>
+</table>
+            {/* <ClassBoardList name="discuss" classId={classId} /> */}
         </div>
     );
 }
