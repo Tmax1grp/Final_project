5571caedfc948967c75a4cd3a7096c1ee2ed5844
jieun <park940313@naver.com>
2021-11-11 22:23:58 +0900

    boardcreate

--- src/Board/BoardCreate.js
+++ src/Board/BoardCreate.js
@@ -1,70 +1,78 @@
-// import React, { Fragment, useState } from 'react';
-// import styles from './Board.module.css';
-// import { CKEditor } from '@ckeditor/ckeditor5-react';
-// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import axios from 'axios';
+import React, { Fragment, useState } from 'react';
+import styles from './Board.module.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import axios from 'axios';
 
-// export default function BoardCreate() {
+export default function BoardCreate() {
 
-//   const classId = window.location.pathname.split('/')[2];
+  const classId = window.location.pathname.split('/')[2];
 
-//   // const [ board, setBoards ] = useState({
-//   //   title: '',
-//   //   content: ''
-//   // });
+  const [ board, setBoards ] = useState({
+    title: '',
+    content: ''
+  });
 
-//   // const handleChangeForm = (e) => {
-//   //   setBoards({
-//   //     ...board,
-//   //     [e.target.name]: e.target.value
-//   //   })
-//   // }
+  const [ boardcontent, setBoardContent ] = useState([]);
 
-//   console.log(board)
-//   // const create = () => {
-//   //   let url = `/notice-service/${classId}/notice`
-//   //   let data = {
-//   //     'title' : board.title,
-//   //     'content' : board.content
-//   //   }
-//   //   var config={
-//   //     headers: {
-//   //       'Content-Type' : 'application/json',
-//   //       'Authorization': sessionStorage.token
-//   //     }
-//   //   }
-//   //   axios.post(url, data, config)
-//   //   .then(res => {
-//   //     alert("성공")
-//   //   }).catch(err => {
-//   //     alert("실패")
-//   //   })
-//   // };
+  const handleChangeForm = (e) => {
+    setBoards({
+      ...board,
+      [e.target.name]: e.target.value
+    })
+  }
 
-//   return (
-//     <Fragment>
-//       <div>
-//         <input className={styles.titleinput} type='text' name="title" placeholder='제목을 입력해주세요' value={title} onChange={handleChangeForm} />
-//         <CKEditor
-//           editor={ClassicEditor}
-//           data={content}
-//           onReady={editor => {
-//             // You can store the "editor" and use when it is needed.
-//             console.log('Editor is ready to use!', editor);
-//           }}
-//           onChange={(event, editor) => {
-//             const data = editor.getData();
-//             console.log({ event, editor, data });
-//           }}
-//           onBlur={(event, editor) => {
-//             console.log('Blur.', editor);
-//           }}
-//           onFocus={(event, editor) => {
-//             console.log('Focus.', editor);
-//           }}
-//         />
-//       </div>
-//       {/* <button onClick={create}>저장</button> */}
-//     </Fragment>
-//   )
-// };
+  console.log(board)
+  const create = () => {
+    let url = `/notice-service/${classId}/notice`
+    let data = {
+      'classId' : classId,
+      'title' : board.title,
+      'content' : board.content,
+      'author' : sessionStorage.userId
+    }
+    var config={
+      headers: {
+        'Content-Type' : 'application/json',
+        'Authorization': sessionStorage.token
+      }
+    }
+    axios.post(url, data, config)
+    .then(res => {
+      alert("성공")
+    }).catch(err => {
+      alert("실패")
+    })
+  };
+
+  return (
+    <Fragment>
+      <div>
+        <input className={styles.titleinput} type='text' name="title" placeholder='제목을 입력해주세요' value={board.title} onChange={handleChangeForm} />
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hi<p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+            setBoards({
+              ...board,
+              content: data
+            })
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </div>
+      <button onClick={create}>저장</button>
+    </Fragment>
+  )
+};
