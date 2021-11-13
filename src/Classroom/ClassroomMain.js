3b9d16fdf7ad60d275e7bb7e468625bf1cf038e8
Sungho Bang <crosstreet74@gmail.com>
2021-11-13 17:09:19 +0900

    tested classroom:member manage

--- src/Classroom/ClassroomMain.js
+++ src/Classroom/ClassroomMain.js
@@ -14,28 +14,29 @@ import ClassBoardHomework from '../Board/ClassBoardAssignment'
 import ClassBoardDiscuss from '../Board/ClassBoardDiscuss'
 import ClassBoardResource from '../Board/ClassBoardReference'
 // import ClassBoardList from '../widgets/ClassBoardList';
+import ClassMemberManage from '../widgets/ClassMemberManage'
 import styles from '../layout/Class.module.css'
 
 export default function ClassroomMain() {
 
   const classId = window.location.pathname.split('/')[2];
-  const [ clsname, setClsname ] = useState([]);
-  
+  const [clsname, setClsname] = useState([]);
+
   useEffect(() => {
     axios.get('/classroom-service/lectures/all')
-    .then(res => {
-      var cnt = 0
-      while (res.data.length !== 0) {
-        if (res.data[cnt].classId == classId){
-          break;
-        } else {
-          cnt += 1
+      .then(res => {
+        var cnt = 0
+        while (res.data.length !== 0) {
+          if (res.data[cnt].classId == classId) {
+            break;
+          } else {
+            cnt += 1
+          }
         }
-      }
-      setClsname(res.data[cnt]);
-    }).catch(err => console.log(err))
+        setClsname(res.data[cnt]);
+      }).catch(err => console.log(err))
   }, [])
-  
+
   return (
     <div>
       <Navmenu />
@@ -63,27 +64,33 @@ export default function ClassroomMain() {
                 <Nav.Item>
                   <Nav.Link eventKey="sixth" className={styles.sidemenu}>자료게시판</Nav.Link>
                 </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="seventh" className={styles.sidemenu}>수강생관리</Nav.Link>
+                </Nav.Item>
               </Nav>
             </Col>
             <Col xl={10} sm={10}>
               <Tab.Content>
                 <Tab.Pane eventKey="first">
-                  <ClassBoardHome classId={classId} content={clsname.content}/>
+                  <ClassBoardHome classId={classId} content={clsname.content} />
                 </Tab.Pane>
                 {/* <Tab.Pane eventKey="second">
                   <ClassBoardCurriculum content={clsname.content}/>
                 </Tab.Pane> */}
                 <Tab.Pane eventKey="third">
-                  <ClassBoardNotice classId={classId}/>
+                  <ClassBoardNotice classId={classId} />
                 </Tab.Pane>
                 <Tab.Pane eventKey="fourth">
-                  <ClassBoardHomework classId={classId}/>
+                  <ClassBoardHomework classId={classId} />
                 </Tab.Pane>
                 <Tab.Pane eventKey="fifth">
-                  <ClassBoardDiscuss classId={classId}/>
+                  <ClassBoardDiscuss classId={classId} />
                 </Tab.Pane>
                 <Tab.Pane eventKey="sixth">
-                  <ClassBoardResource classId={classId}/>
+                  <ClassBoardResource classId={classId} />
+                </Tab.Pane>
+                <Tab.Pane eventKey="seventh">
+                  <ClassMemberManage classId={classId} />
                 </Tab.Pane>
               </Tab.Content>
             </Col>
---
+++ src/widgets/ClassMemberManage.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+import ClassMemberItem from './ClassMemberItem';
+
+export default function ClassMemberManage({ classId }) {
+    const [members, setMembers] = useState(null);
+
+    useEffect(() => {
+        axios.get('/lecture-service/students', {
+            params: {
+                classroomId: classId
+            }
+        }
+        ).then(res => {
+            console.log(res.data)
+            setMembers(res.data);
+        }).catch((err) =>
+            console.log(err)
+        )
+    }, [])
+    return (
+        <>
+            <Table responsive="sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>회원이름</th>
+                        <th>수강상태</th>
+                        <th>수강신청승인</th>
+                        <th>수강취소처리</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        members !== null ?
+                            members.filter(member => member.status == 0 || member.status == 1)
+                                .map(member => {
+                                    return (<ClassMemberItem classId={classId} key={member.userId} member={member} />);
+                                })
+                            :
+                            <>사용자 데이터를 불러올 수 없습니다.</>
+                    }
+                </tbody>
+            </Table>
+
+        </>
+    );
+}
