51aef57f38ae700664e596fb857ef8fa76273de7
Alexhwang95 <mdok3488@naver.com>
2021-11-13 18:50:10 +0900

    검색

--- src/Board/ClassBoardReference.js
+++ src/Board/ClassBoardReference.js
@@ -1,60 +1,107 @@
-import React,{useEffect, useState} from 'react';
-import ClassBoardList from '../widgets/ClassBoardList'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import ClassBoardSearchMenu from './ClassBoardSearchMenu';
 
 // TODO: 한 페이지 게시글 최대 개수 지정 혹은 스크롤링
-export default function ClassBoardReference({classId}) {
-
+export default function ClassBoardReference({ classId }) {
+  const [keyword, setKeyword] = useState({
+    keywordType: 0,
+    keywordValue: null
+  });
+  const [filtered, setFiltered] = useState([]);
   const [classes, setClasses] = useState([]);
 
   useEffect(() => {
     axios.get(`/reference-service/${classId}/reference/all`)
-    .then(res => {
-      // console.log(res.data)
-      setClasses(res.data);
-    })
-    .catch((err) =>
-      console.log(err)
-    )
-  },[classId])
+      .then(res => {
+        // console.log(res.data);
+        setClasses(res.data);
+        setFiltered(res.data);
+      })
+      .catch((err) =>
+        console.log(err)
+      )
+    // setFiltered(classes);
+  }, [classId])
 
-  const classeslist = classes.map((clas) => {
+  useEffect(() => {
+    if (keyword.keywordValue != null) {
+      let searchType = parseInt(keyword.keywordType);
+      let searchValue = keyword.keywordValue;
+      // console.log("searchType: " + searchType);
+      // console.log("searchValue: " + searchValue);
+
+      switch (searchType) {
+        case 0: {
+          setFiltered(classes.filter(item => item.author.includes(searchValue)));
+          break;
+        }
+        case 1: {
+          setFiltered(classes.filter(item => item.title.includes(searchValue)));
+          break;
+        }
+        default: {
+          console.log("ERROR: Invalid Search Keyword Type");
+          // setFiltered(classes);
+          break;
+        }
+      }
+    }
+  }, [keyword])
+
+  // 게시글 목록
+  var classesList = filtered.map((clas) => {
+    const enterreferencedetail = () => {
+      let clickCnt = 1
+      axios.get(`/reference-service/${classId}/reference/${clas.referenceId}`, {
+        params: {
+          clickCnt: 0
+        }
+      })
+    }
+    // 게시글 작성일 raw 데이터 편집
+    if (clas.createDate != null) {
+      var dateRaw = clas.createDate;
+      var createDate = '';
+      var createTime = '';
+      createDate = dateRaw.split("T")[0];
+      createTime = dateRaw.split("T")[1];
+      createTime = createTime.split(":")[1] + ":" + createTime.split(":")[2];
+    }
+
     return (
       <tr>
-        <th scope="row">{clas.noticeId}</th>
-        <td>{clas.title}</td>
+        <th scope="row">{clas.referenceId}</th>
+        <td>
+          <button onClick={enterreferencedetail}>
+            <a href={`/reference-service/${classId}/reference/${clas.referenceId}`}>{clas.title}</a>
+          </button>
+        </td>
         <td>{clas.author}</td>
-        <td>{clas.createDate}</td>
+        <td>{createDate + " " + createTime}</td>
         <td>{clas.clickCnt}</td>
-      </tr>          
-      )
-    }
-  )
+      </tr>
+    )
+  })
 
   return (
     <div>
       <div className="row">
-        <div className="col-6"></div>
-        <div className="col-3">
-          <table className="table col-3">
-            <tr>
-              <td>
-                <input type="text" size="25" className="input-sm" placeholder="검색"/>
-              </td>
-            </tr>
-          </table>
+        <div className="col-2">
+          <Link to={`/boardcreate/${classId}`} params={classId}>
+            <i class="fas fa-pen-square fa-2x" style={{ color: "black" }}></i>
+          </Link>
         </div>
-        <div className="col-3">
-          <select className="form-select col-3" aria-label="Default select example">
-            <option selected>전체선택</option>
-            <option value="1">제목</option>
-            <option value="2">작성자</option>
-          </select>
+        <div className="col-10">
+          <ClassBoardSearchMenu keyword={keyword} setKeyword={setKeyword} />
         </div>
       </div>
+
       <table className="table">
-        <thead>
-          <tr>  
+        <thead className="thead-dark">
+          <tr>
             <th scope="col">번호</th>
             <th scope="col">제목</th>
             <th scope="col">작성자</th>
@@ -63,7 +110,11 @@ export default function ClassBoardReference({classId}) {
           </tr>
         </thead>
         <tbody>
-          {classeslist}
+          {filtered.length > 0 ?
+            classesList
+            :
+            <>검색된 게시글이 없습니다!</>
+          }
         </tbody>
       </table>
       {/* <ClassBoardList name="reference" classId={classId} /> */}
